c7c8a923a323a2904e7c5ff7a78f82e5f74cd39d
Argiris <argdimitriou@gmail.com>
2021-06-03 15:04:07 +0300

    ocr captcha fragments and classes

---
+++ app/src/main/java/com/example/mycatcha/MainActivity4.java
@@ -0,0 +1,70 @@
+package com.example.mycatcha;
+
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.Bundle;
+import com.example.mycatcha.databinding.ActivityMain4Binding;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMain4Binding binding;
+
+    ImageView im;
+    Button btn;
+    TextView ans;
+
+    private String cAnswer = "PQJRYD";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMain4Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        // OCR Captcha generation and check
+        im = (ImageView)findViewById(R.id.imageView3);
+        btn = (Button)findViewById(R.id.nextButton);
+        ans = (EditText)findViewById(R.id.textView3);
+
+        int image = getIntent().getIntExtra("image",R.drawable.images);
+        im.setImageResource(image);
+
+        btn.setEnabled(false);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v){
+                if (ans.toString() == cAnswer) {
+                    btn.setEnabled(true);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
