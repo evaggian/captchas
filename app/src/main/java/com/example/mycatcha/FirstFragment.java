4d778ee0392ee3b2cc7ba61b9138033023c1bf00
Evangelia Giannikou <eva@eco-movement.com>
2021-06-09 12:23:32 +0200

    Revert "work in progress"
    This reverts commit 5852715d

--- app/src/main/java/com/example/mycatcha/FirstFragment.java
+++ app/src/main/java/com/example/mycatcha/FirstFragment.java
@@ -21,8 +21,13 @@ public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
 
-    private final Date date = new Date();
-    long start = date.getTime();
+    Button btn;
+    EditText ans;
+    public String value;
+
+    private String cAnswer = "PQJRYD";
+    private Date date = new Date();
+    long start = (long) date.getTime();
 
     @Override
     public View onCreateView(
@@ -38,13 +43,45 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.nextButton.setOnClickListener(view1 -> {
-            Date date2 = new Date();
-            long end = (long) date2.getTime();
-            long timeSpent = end - start;
-            OCRActivity.myRef.child("OCR Captcha time spent:").setValue(timeSpent);
-            NavHostFragment.findNavController(FirstFragment.this)
-                    .navigate(R.id.action_FirstFragment_to_SUSFirstFragment);
+        MainActivity mainActivity = new MainActivity();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://mycaptcha-1e0f4-default-rtdb.europe-west1.firebasedatabase.app/");
+        DatabaseReference myRef = database.getReference("Random ID:" + MainActivity.getRandomID());
+        myRef.child("Phone model:").setValue(MainActivity.getDeviceName());
+        myRef.child("Phone resolution:").setValue(getScreenResolution());
+
+        // OCR Captcha generation and check
+        /*binding.nextButton.setEnabled(false);
+        ans = binding.textView3;
+        ans.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.equals(cAnswer)){
+                    Log.i("Answer is:", s.toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });*/
+
+        binding.nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Date date2 = new Date();
+                long end = (long) date2.getTime();
+                long timeSpent = end - start;
+                myRef.child("OCR Captcha time spent:").setValue(timeSpent);
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SUSFirstFragment);
+            }
         });
     }
 
@@ -52,6 +89,13 @@ public class FirstFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public String getScreenResolution() {
+        int width = getResources().getConfiguration().screenWidthDp;
+        int height = getResources().getConfiguration().screenHeightDp;
+        int density = getResources().getDisplayMetrics().densityDpi;
+        return "{" + width + "," + height + "," + density + "}";
     }
 
 }
--- app/src/main/java/com/example/mycatcha/MainActivity.java
+++ app/src/main/java/com/example/mycatcha/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.mycatcha;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,10 +22,45 @@ public class MainActivity extends AppCompatActivity {
         simpleImageView.getLayoutParams().height = 300;
         simpleImageView.getLayoutParams().width = 300;
 
+//        Logging the phone model in the console.
+
+
+        // Logging the phone's height and width:
+        //Log.i("Screen width & height:", getScreenResolution());
     }
 
     public void submitMainButtonHandler(View view) {
         Intent intent = new Intent(this, OCRActivity.class);
         startActivity(intent);
+    }
+
+//    Retrieving the device manufacturer and name.
+    public static String getDeviceName() {
+        String manufacturer = Build.MANUFACTURER;
+        String model = Build.MODEL;
+        if (model.toLowerCase().startsWith(manufacturer.toLowerCase())) {
+            return capitalize(model);
+        } else {
+            return capitalize(manufacturer) + " " + model;
+        }
+    }
+
+    //    Capitalizing the first letter of the model or manufacturer.
+    private static String capitalize(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        char first = s.charAt(0);
+        if (Character.isUpperCase(first)) {
+            return s;
+        } else {
+            return Character.toUpperCase(first) + s.substring(1);
+        }
+    }
+
+    static int random = new Random().nextInt(6000);
+    public static String getRandomID(){
+        Log.i("Random:", String.valueOf(random));
+        return String.valueOf(random);
     }
 }
